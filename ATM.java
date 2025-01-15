5c9ab555ae53a12f6b03214a362d9c8b724d5824
swatilohar <swatilohar2261@gmail.com>
2025-01-15 13:14:38 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,14 @@
+class ATM{
+public void checkBalance()
+{
+	System.out.println("Check Balance ");
+}
+
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to SBI");
+		ATM atm=new ATM();
+		atm.checkBalance();
+
+	}
+}
