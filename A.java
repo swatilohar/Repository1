9de5f586e28125a7c9d9289164ae9f8c1ec03464
swatilohar <swatilohar2261@gmail.com>
2025-01-15 13:13:33 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,20 @@
+class A{
+int x=10;
+
+public void m1()
+{
+System.out.println("m1 method");
+int y=20;
+System.out.println(y);
+}
+
+public static void main(String[] args)
+{
+System.out.println("Main method");
+A a= new A();
+System.out.println(a.x);
+a.m1();
+
+
+}
+}
